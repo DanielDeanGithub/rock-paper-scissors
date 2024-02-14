0653a7956bdc9e209450b3e5479798cf1d2ff95a
DanDean191 <github-new@inboxrefresh.com>
2024-02-14 18:03:05 +0000

    Added check for valid userInput

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,4 +2,6 @@ console.log('hi');
 
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') return userInput;
+    console.log('Error: Invaild choice.')
 };
