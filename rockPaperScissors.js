1f5decddd3210bfb31e8dd11d4a37ab6c3995365
DanDean191 <github-new@inboxrefresh.com>
2024-02-14 18:08:53 +0000

    Created getComputerChoice function to calculate computer choice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,3 +5,19 @@ const getUserChoice = userInput => {
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') return userInput;
     console.log('Error: Invaild choice.')
 };
+
+const getComputerChoice = () => {
+    const rng = Math.floor(Math.random()*3);
+
+    switch (rng) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper'
+            break
+        default:
+            return 'scissors'
+            break;
+    }
+}
